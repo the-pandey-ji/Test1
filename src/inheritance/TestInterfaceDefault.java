f07870cab62b195777f8b104cc95992402027cbc
the-pandey-ji <sonu.pandey_cs18@gla.ac.in>
2020-07-29 23:47:21 +0530

    static and efault method in interface

--- src/inheritance/TestInterfaceDefault.java
+++ src/inheritance/TestInterfaceDefault.java
@@ -1,4 +1,8 @@
 package inheritance;
+//The interface in Java is a mechanism to achieve abstraction. There can be only abstract methods in
+// the Java interface, not method body.
+// It is used to achieve abstraction and multiple inheritance in Java.
+
 interface Drawable{
     void draw();
     default void msg(){System.out.println("default method");} // we can use method body in interface by default method
