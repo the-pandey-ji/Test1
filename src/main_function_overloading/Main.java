71d4400f99128ef187d54279b363af19cac01ff4
the-pandey-ji <sonu.pandey_cs18@gla.ac.in>
2020-07-29 23:25:55 +0530

    class without main function

---
+++ src/main_function_overloading/Main.java
@@ -0,0 +1,16 @@
+package main_function_overloading;
+
+public class Main {
+        public static void main(String[] args) {
+            System.out.println("main(String[] args)");
+        }
+
+        public static void main(String arg1) {
+            System.out.println("main(String arg1)");
+        }
+
+        public static void main(String arg1, String arg2) {
+            System.out.println("main(String arg1, String arg2)");
+        }
+
+}
---
+++ src/main_function_overloading/StaticInitializationBlock.java
@@ -0,0 +1,11 @@
+package main_function_overloading;
+
+public class StaticInitializationBlock {
+
+        static{
+            System.out.println("class without a main method");
+            System.exit(0);
+        }
+    }
+    //works in java 7 or java 1.6 newer versions does not allow this
+
